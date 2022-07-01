327d91371e16ef52b51c5da62ef536bfb5d98c19
Anthony Roach <aroach@iconbuild.com>
2022-07-01 16:42:21 -0400

    Return status code 400 for validation errors
    Errors in validation mean the client sent something invalid, so
    the response status code should be 400 (Bad Request) rather
    than the default status code of 500 (Internal Server Error).

--- test/errors.js
+++ test/errors.js
@@ -13,11 +13,12 @@ t.test('errors', t => {
       t.plan(1)
 
       t.test('should print a validation error to string', t => {
-        t.plan(1)
+        t.plan(2)
 
         const error = new errors.MER_VALIDATION_ERR_FAILED_VALIDATION('some message', [])
 
         t.same(error.toString(), 'ValidationError [MER_VALIDATION_ERR_FAILED_VALIDATION]: some message')
+        t.equal(error.statusCode, 400)
       })
     })
   })
